88fab8d5ac7dac846a0e5cd3ee94f059129f3570
Tahj-Roehampton <johnsont8@roehampton.ac.uk.com>
2023-02-08 19:09:14 +0000

    Week3 adding existing file

---
+++ scriptpg2.js
@@ -0,0 +1,9 @@
+//function for the first button
+//refreshes screen and shows text
+function msg() {document.write("You clicked the button!");}
+
+
+//function for the second button
+//changging the text below
+//using the 'id' attribute in HTML to identify a unique part of a page
+function change() {document.getElementById("my_text").innerHTML = "You clicked me"; }
